8fc00bf23c15bd6b322d32be3f12da768b039e31
ProNoice <robex.omar@gmail.com>
2024-03-31 16:11:37 +0200

    Initial commit

---
+++ src/main/java/net/nexus/metaldetectors/MetalDetectorsDataGenerator.java
@@ -0,0 +1,11 @@
+package net.nexus.metaldetectors;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class MetalDetectorsDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
